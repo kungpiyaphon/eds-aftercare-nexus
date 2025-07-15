6f8831bde56afc96a846466ea76416465d4d8e78
Piyaphon Waharak <piyaphon.w@eds.local>
2025-07-15 14:21:58 +0700

    feat(file): added logo partner

--- src/components/BrandCarouselSection.tsx
+++ src/components/BrandCarouselSection.tsx
@@ -1,22 +1,33 @@
 import React from "react";
 
 const brandPartners = [
-  { src: "/logos/samsung.svg", alt: "Samsung" },
-  { src: "/logos/lg.svg", alt: "LG" },
-  { src: "/logos/sony.svg", alt: "Sony" },
-  { src: "/logos/panasonic.svg", alt: "Panasonic" },
-  { src: "/logos/huawei.svg", alt: "Huawei" },
-  { src: "/logos/xiaomi.svg", alt: "Xiaomi" },
-  { src: "/logos/sharp.svg", alt: "Sharp" },
+  { src: "/logos/samsung.png", alt: "Samsung" },
+  { src: "/logos/xiaomi.png", alt: "Xiaomi" },
+  { src: "/logos/brother.png", alt: "Brother" },
+  { src: "/logos/hisense.png", alt: "Hisense" },
+  { src: "/logos/sunvending.png", alt: "Sun Vending" },
+  { src: "/logos/fujifilm.png", alt: "Fujifilm" },
+  { src: "/logos/samsungsds.png", alt: "Samsung SDS" },
+  { src: "/logos/lenovo.png", alt: "Lenovo" },
+  { src: "/logos/centerm.png", alt: "Centerm" },
+  { src: "/logos/dieboldnixdorf.png", alt: "Diebold Nixdorf" },
+  { src: "/logos/sharp.png", alt: "Sharp" },
+  { src: "/logos/ncr.png", alt: "NCR" },
+  { src: "/logos/huntkey.png", alt: "Huntkey" },
 ];
 
 const retailPartners = [
-  { src: "/logos/bigc.svg", alt: "Big C" },
-  { src: "/logos/tesco.svg", alt: "Tesco Lotus" },
-  { src: "/logos/powerbuy.svg", alt: "PowerBuy" },
-  { src: "/logos/bananait.svg", alt: "Banana IT" },
-  { src: "/logos/advice.svg", alt: "Advice" },
-  { src: "/logos/jmart.svg", alt: "Jmart" },
+  { src: "/logos/jaymart.png", alt: "Jaymart" },
+  { src: "/logos/om7.png", alt: "Com 7" },
+  { src: "/logos/tgfone.png", alt: "TG Fone" },
+  { src: "/logos/wistron.png", alt: "Wistron" },
+  { src: "/logos/powerbuy.png", alt: "Power Buy" },
+  { src: "/logos/itcity.png", alt: "IT City" },
+  { src: "/logos/asurion.png", alt: "Asurion" },
+  { src: "/logos/sunday.png", alt: "Sunday" },
+  { src: "/logos/bolttech.png", alt: "Bolttech" },
+  { src: "/logos/chubb.png", alt: "Chubb" },
+  { src: "/logos/tn.png", alt: "TN" },
 ];
 
 const BrandCarouselSection = () => {
@@ -48,7 +59,7 @@ const BrandCarouselSection = () => {
         <div>
           <h3 className="text-2xl font-bold text-primary mb-6 text-center">Retail Partners</h3>
           <div className="relative w-full overflow-hidden">
-            <div className="flex gap-6 animate-slide-right w-max">
+            <div className="flex gap-6 animate-slide-left w-max">
               {[...retailPartners, ...retailPartners].map((logo, i) => (
                 <div
                   key={i}
