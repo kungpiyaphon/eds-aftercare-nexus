d6e490769aebe3728287e46cc8621942d9afd0b5
kungpiyaphon <kungpiyaporn.0452@gmail.com>
2025-07-21 08:50:04 +0700

    fix(img): changed name and set image

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,7 +11,8 @@ const samsungBranches = [
     address: "66 Soi Ananmai 39, 24 Srinagarindra Rd, Suan Luang, Bangkok 10250",
     // phone: "+66 2 345 6780",
     lineUrl: "https://lin.ee/LRZBpYn",
-    mapUrl: "https://maps.app.goo.gl/MMQfJNNaTiBmnK5K9"
+    mapUrl: "https://maps.app.goo.gl/m7TVFeEKAhGKB9GQ9",
+    img:"HQ"
   },
   {
     name: "Samsung MBK Center",
@@ -19,7 +20,8 @@ const samsungBranches = [
     address: "MBK Shopping Center, Room No. 2S-F5 F6, 5th Floor, No. 444, Phaya Thai Rd, Pathum Wan, Bangkok 10330",
     // phone: "+66 2 123 4567",
     lineUrl: "https://lin.ee/v75n74P",
-    mapUrl: "https://maps.app.goo.gl/jBmtTpVgY1T7vrFn7"
+    mapUrl: "https://maps.app.goo.gl/jBmtTpVgY1T7vrFn7",
+    img:"MBK"
   },
   {
     name: "Samsung Future Park Rangsit",
@@ -27,7 +29,8 @@ const samsungBranches = [
     address: "Future Park Rangsit, 3rd Floor, No. 94 Phahonyothin Rd, Prachathipat, Thanyaburi, Pathum Thani 12130",
     // phone: "+66 2 234 5678",
     lineUrl: "https://lin.ee/LTY7JaU",
-    mapUrl: "https://maps.app.goo.gl/8usBALymeczhfCuj7"
+    mapUrl: "https://maps.app.goo.gl/8usBALymeczhfCuj7",
+    img:"FPR"
   },
   {
     name: "Samsung Seacon Bangkae",
@@ -35,7 +38,8 @@ const samsungBranches = [
     address: "Seacon Bang Khae, Room No. 357, 3rd Floor, No. 607 Petchkasem Road, Bang Wa, Bangkok 10160",
     // phone: "+66 2 345 6789",
     lineUrl: "https://lin.ee/xrLC3pt",
-    mapUrl: "https://maps.app.goo.gl/7ExPaacjEKDc32oU9"
+    mapUrl: "https://maps.app.goo.gl/7ExPaacjEKDc32oU9",
+    img:"SBK"
   },
   {
     name: "Samsung The Mall Bangkae",
@@ -43,7 +47,8 @@ const samsungBranches = [
     address: "518 Phet Kasem Rd, Bang Khae Nuea, Phasi Charoen, Bangkok 10600",
     // phone: "+66 2 456 7890",
     lineUrl: "https://lin.ee/LbH0Msv",
-    mapUrl: "https://maps.app.goo.gl/guzEF1H7gTz1eaWs8"
+    mapUrl: "https://maps.app.goo.gl/guzEF1H7gTz1eaWs8",
+    img:"TMB"
   },
   {
     name: "Samsung Rayong",
@@ -51,7 +56,8 @@ const samsungBranches = [
     address: "The Design Village 18/49, Mueang Rayong District, Rayong 21000",
     // phone: "+66 38 123 456",
     lineUrl: "https://lin.ee/CuMPhur",
-    mapUrl: "https://maps.app.goo.gl/Mxhy7GxNZMUoqgkP7"
+    mapUrl: "https://maps.app.goo.gl/Mxhy7GxNZMUoqgkP7",
+    img:"RY"
   },
   {
     name: "Samsung Ayutthaya",
@@ -59,7 +65,8 @@ const samsungBranches = [
     address: "235, Moo 5 Rotchana Rd, Tambon Phai Ling, Phra Nakhon Si Ayutthaya District, Phra Nakhon Si Ayutthaya 13000",
     // phone: "+66 35 234 567",
     lineUrl: "https://lin.ee/ZPvdYqvu",
-    mapUrl: "https://maps.app.goo.gl/8JBNzHKrimzMg4Gk9"
+    mapUrl: "https://maps.app.goo.gl/8JBNzHKrimzMg4Gk9",
+    img:"AYY"
   },
   {
     name: "Samsung Sampheng 2",
@@ -67,7 +74,8 @@ const samsungBranches = [
     address: "88, 150-151 Kanlapaphruek Rd, Khwaeng Bang Khae, Bang Khae, Bangkok 10160",
     // phone: "+66 2 345 6780",
     lineUrl: "https://lin.ee/HMbSFlS",
-    mapUrl: "https://maps.app.goo.gl/H5m8jxdY8m82BbNt8"
+    mapUrl: "https://maps.app.goo.gl/H5m8jxdY8m82BbNt8",
+    img:"SP2"
   },
   {
     name: "Xiaomi Authorized Service Center",
@@ -75,7 +83,8 @@ const samsungBranches = [
     address: "163/44 Moo 3, Phra Nakhon Si Ayutthaya District, Phra Nakhon Si Ayutthaya 13000",
     // phone: "+66 2 345 6780",
     lineUrl: "https://lin.ee/LRZBpYn",
-    mapUrl: "https://maps.app.goo.gl/6GoiaPGt6gfEbork9"
+    mapUrl: "https://maps.app.goo.gl/6GoiaPGt6gfEbork9",
+    img:"XAY"
   }
 ];
 
@@ -97,7 +106,7 @@ const Contact = () => {
               <Card key={index} className="hover:shadow-elegant transition-all duration-300 overflow-hidden">
                 {/* Branch Image */}
                 <img
-                  src={`/images/branches/${index + 1}.jpg`} // ตัวอย่าง path: public/images/branches/1.jpg
+                  src={`/images/branches/${branch.img}.jpg`} // ตัวอย่าง path: public/images/branches/1.jpg
                   alt={`Photo of ${branch.name}`}
                   className="w-full h-48 object-cover rounded-t-lg"
                 />
